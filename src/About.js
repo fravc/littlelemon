6a34082d47b8560f493689d330c5d50fdfb91121
julia.mota <julia.mota@totvs.com.br>
2023-07-05 21:18:30 -0300

    styling the about page

--- src/About.js
+++ src/About.js
@@ -5,17 +5,15 @@ import ImageMarioAdrian from '../src/assets/images/ImageAbout.png';
 function AboutSection (){
     return(
         <div className="AboutContainer">
-            <h2>Little Lemon</h2>
-            <h3>Chicago</h3>
-            <spam>
-                <p>
-                    Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
-                    Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.  
-                </p>
+            <div className="text-column">
+                <h2>Little Lemon</h2>
+                <h3>Chicago</h3>
+                    <p>
+                        Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+                        Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.  
+                    </p>
+            </div>
                 <img alt="mario and adrian cooking" src={ImageMarioAdrian} className='ImageAbout'/>
-            </spam>
-            
-
         </div>
     )
 }
